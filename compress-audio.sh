dd486a8aa15a8da611d93653823b73ea3a1638be
Michel <sa6mwa@radiohorisont.se>
2019-11-05 20:44:49 +0100

    Better sox compand settings

--- compress-audio.sh
+++ compress-audio.sh
@@ -1,22 +1,31 @@
 #!/bin/sh
-#COMPAND="0.02,0.20 5:-60,-40,-10 -5 -90 0.1"
-#COMPAND="0.3,5 6:-70,-60,-20 -10 -6 0.2"
-#COMPAND="0.3,5 6:-70,-60,-10 -10 -6 0.2"
-#COMPAND="0.001,5 6:-70,-60,-10 -10 -6 0.2"
-COMPAND="0.00001,5 6:-70,-60,-3 -1 -6 0.2"
+set -e
+
+ATTACK="0.000001"
+DECAY="0.8"
+ROUND="6"
+SILENCE_BELOW="-30"
+GAIN="-1"
+INITIAL_ASSUMED_LEVEL="${SILENCE_BELOW}"
+ADJUSTER_DELAY="0.001"
+
+# compand attack1,decay1 soft-knee-dB:in-dB1,out-dB1 gain initial_assumed_signal_level volume_adjuster_delay
+COMPAND="${ATTACK},${DECAY} ${ROUND}:${SILENCE_BELOW},0 ${GAIN} ${INITIAL_ASSUMED_LEVEL} ${ADJUSTER_DELAY}"
+
 if [ $# -lt 1 ]; then
-  echo "usage: $0 wavfile1.wav wavfile2.wav..."
-  echo "will produce compressed-wavfile1.wav, etc"
+  echo "Usage: $0 wavfile1.wav wavfile2.wav..."
+  echo "Will produce compressed-wavfile1.wav, etc."
+  echo "Assuming silence below ${SILENCE_BELOW} dB after normalization."
   exit 1
 fi
 for f in $*
 do
-  echo "$f: applying highpass filter" &&
-  sox $f highpass-$f gain -n -3 highpass 100 &&
-  echo "$f: normalizing" &&
-  sox --norm=-2 highpass-$f normalized-$f &&
-  rm -f highpass-$f &&
-  echo "$f: applying compression" &&
-  sox normalized-$f compressed-$f compand $COMPAND &&
+  echo "$f: applying highpass filter"
+  sox $f highpass-$f channels 1 gain -n -3 highpass 100
+  echo "$f: normalizing"
+  sox --norm=-1 highpass-$f normalized-$f
+  rm -f highpass-$f
+  echo "$f: applying compression"
+  sox normalized-$f compressed-$f compand $COMPAND
   rm -f normalized-$f
 done
