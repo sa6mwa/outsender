84031cc59e58c7764aca8dde64a76ec392d3b261
Michel <sa6mwa@radiohorisont.se>
2020-06-16 01:00:54 +0200

    Modmic compander

---
+++ compress-audio-modmic.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+set -e
+
+ATTACK="0.00001"
+DECAY="3"
+ROUND="6"
+SILENCE_BELOW="-42"
+START_COMPRESS_AT="-30"
+COMPRESS_TO="0"
+GAIN="-1"
+INITIAL_ASSUMED_LEVEL="${SILENCE_BELOW}"
+#INITIAL_ASSUMED_LEVEL="${START_COMPRESS_AT}"
+ADJUSTER_DELAY="0.001"
+HIGHPASS="100"
+TREBLE="+12"
+TREBLEQRG="5k"
+BASS="-3"
+BASSQRG="300"
+
+
+# compand attack1,decay1 soft-knee-dB:in-dB1,in-dB2 gain initial_assumed_signal_level volume_adjuster_delay
+# 2nd parameter, 4 means in-dB1,out-dB1,in-dB2,out-dB2
+# 2nd parameter, 3 means in-dB1,in-dB2,out-dB2 (out-dB1 will be the same as in-dB1, no compression)
+# 2nd parameter, 2 means in-dB1,in-dB2 (out-dBn will be the same as in-dBn)
+COMPAND="${ATTACK},${DECAY} ${ROUND}:${SILENCE_BELOW},${SILENCE_BELOW},${START_COMPRESS_AT},${COMPRESS_TO} ${GAIN} ${INITIAL_ASSUMED_LEVEL} ${ADJUSTER_DELAY}"
+
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 wavfile1.wav wavfile2.wav..."
+  echo "Will produce compressed-wavfile1.wav, etc."
+  echo "Assuming silence below ${SILENCE_BELOW} dB after normalization."
+  exit 1
+fi
+for f in $*
+do
+  echo "$f: applying highpass filter and treble gain"
+  sox -G $f highpass-$f channels 1 highpass $HIGHPASS treble $TREBLE ${TREBLEQRG} bass $BASS ${BASSQRG}
+  echo "$f: applying compression"
+  sox highpass-$f compressed-$f compand $COMPAND
+  rm -f highpass-$f
+done
