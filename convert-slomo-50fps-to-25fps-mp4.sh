d6d6c604735ef914efd91134d12982310dbcc78a
Michel <sa6mwa@radiohorisont.se>
2020-06-30 20:00:14 +0200

    slomo conversion

---
+++ convert-slomo-50fps-to-25fps-mp4.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+FPS=25
+#CRF 23 is the default, 17 is visually lossless, -6 is double the bitrate
+CRF=19
+# medium (default), fast, faster, veryfaster, superfast, ultrafast
+PRESET=superfast
+GOP=50
+BF=2
+EXTRA="-tune fastdecode"
+if [ $# -lt 1 ]; then
+  echo "usage: $0 input1.avi input2.mts ..."
+  echo "will produce ${FPS}fps-slomo-input1.avi.mp4 and ${FPS}fps-slomo-input2.mts.mp4"
+  echo "in the current directory"
+  exit
+fi
+for f in $*
+do
+  OUTPUT="${FPS}fps-slomo-$(basename $f).mp4"
+  set -x
+  ffmpeg -i "$f" -vf setpts=2*PTS -r $FPS -c:v libx264 -g $GOP -bf $BF -an -crf $CRF -preset $PRESET $EXTRA "${OUTPUT}"
+  set +x
+done
