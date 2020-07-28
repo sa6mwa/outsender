ce62312ebf2fbf83b549b1467e88ebec8028c7ef
Michel <sa6mwa@radiohorisont.se>
2020-07-28 16:00:54 +0200

    Delay video by 0.08 seconds for better audio sync of DJI 1080p 25fps RockSteady video, and less denoising

--- convert-dji.sh
+++ convert-dji.sh
@@ -7,7 +7,7 @@ PRESET=ultrafast
 GOP=12
 BF=2
 ABR=384k
-VF="eq=saturation=1.65,curves=m='0/0 0.5/0.4 1/1',removegrain=4,hqdn3d=10:8"
+VF="eq=saturation=1.65,curves=m='0/0 0.5/0.4 1/1',removegrain=4,hqdn3d=6"
 # VF curves at sunset m='0/0 0.5/0.6 1/1'
 AFILTER="aresample=async=1,\
 equalizer=f=12500:width_type=h:width=2000:g=-12,\
@@ -15,10 +15,11 @@ equalizer=f=16000:width_type=h:width=3500:g=-12,\
 equalizer=f=20000:width_type=h:width=4000:g=-36,\
 compand=attacks=.001:decays=.2:points=-90/-90|-48/-30|-30/-12|-12/-6|-6/-6|0/-6|20/-6:soft-knee=6"
 if [ $# -lt 1 ]; then
-  echo "usage: $0 input1.avi input2.mts ..."
-  echo "intended to render video taken with DJI Osmo Action for fast editing in Blender"
-  echo "will produce blended-input1.mp4"
-  echo "in the current directory"
+  echo "Usage: $0 input1.avi input2.mts ..."
+  echo "Intended to transcode 1080p 25fps RockSteady video from DJI Osmo Action"
+  echo "for fast editing in Blender."
+  echo "Will produce blended-input1.mp4 in current directory."
+  echo "Video is delayed 0.08 seconds (2 frames out of 25) from audio (syncfix)."
   exit
 fi
 set -x
@@ -26,7 +27,8 @@ for f in $*
 do
   bname="$(basename $f)"
   OUT="blended-${bname%.*}.mp4"
-  ffmpeg -y -i "$f" -r 25 \
+  ffmpeg -y -i "$f" -itsoffset 0.08 -i "$f" -map 0:a -map 1:v \
+  -r 25 \
   -pix_fmt yuv420p -colorspace bt709 -color_trc bt709 -color_primaries bt709 \
   -color_range tv \
   -vf "$VF" -c:v libx264 -preset $PRESET -profile:v high -g 12 -bf 2 -crf 17 \
