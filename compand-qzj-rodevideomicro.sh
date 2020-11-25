86f8ce4e854e3063146a5231edcbf32ae2a44864
SA6MWA <sa6mwa@radiohorisont.se>
2020-11-25 04:03:27 +0100

    broadcastish eq for rode videomicro

--- compand-qzj-rodevideomicro.sh
+++ compand-qzj-rodevideomicro.sh
@@ -12,11 +12,33 @@ do
   ffmpeg -y -i "$f" -vn -ac 2 -filter_complex "\
 pan=stereo|c0<.5*c0+.5*c1|c1<.5*c0+.5*c1,\
 deesser,\
+equalizer=f=250:t=o:w=1:g=-8,\
+equalizer=f=500:t=o:w=1:g=-8,\
+equalizer=f=1000:t=o:w=1:g=-7,\
+equalizer=f=2000:t=o:w=1:g=-3,\
+equalizer=f=4000:t=o:w=1:g=-6,\
+equalizer=f=6000:t=o:w=.5:g=-6,\
 highpass=f=100,\
-equalizer=f=300:t=q:w=1:g=-6,\
-equalizer=f=12000:t=q:w=2:g=2,\
-compand=attacks=.0001:decays=.2:points=-90/-900|-80/-90|-50/-50|-30/-9|-2/-2|0/-2|20/-2:soft-knee=3" \
+lowpass=f=13000,\
+compand=attacks=.0001:decays=.25:points=-90/-900|-80/-90|-50/-50|-36/-6|0/-1|20/-1:soft-knee=6,\
+alimiter" \
 "$OUT"
 done
 
+## fft denoise, not necessary with this mic/preamp combo
 # afftdn=nt=w:nr=12
+
+## octaves, center qrg
+# 63 125 250 500 1000 2000 4000 8000 16000
+## octaves, where the center qrg covers the low and high qrgs
+# 20   - 31.5 - 45
+# 45   - 63   - 90
+# 90   - 125  - 175
+# 175  - 250  - 350
+# 350  - 500  - 700
+# 700  - 1k   - 1.4k
+# 1.4k - 2k   - 2.8k
+# 2.8k - 4k   - 6k
+# 6k   - 8k   - 12k
+# 12k  - 16k  - 20k
+
