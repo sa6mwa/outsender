28577fe1a89f7b18ccb4cecf0dd636971ebef0d3
SA6MWA <sa6mwa@radiohorisont.se>
2021-04-06 22:22:15 +0200

    Wait for stable file size before start rendering

--- youtubize.sh
+++ youtubize.sh
@@ -11,11 +11,30 @@ if [ $# -lt 1 ]; then
   echo "usage: $0 input ..."
   echo "Intended to take a matroska mkv from Blender to publish on YouTube."
   echo "Assumes 25 fps."
+  echo ""
+  echo "Wait for stable file size was added to start youtubization as soon as Blender"
+  echo "has finished rendering the pre-master matroska mkv. Usage example is to"
+  echo "start rendering in Blender, start this script on the (growing) mkv file"
+  echo "and have the computer automatically power down when done..."
+  echo "example: $0 file-being-rendered.mkv ; poweroff"
   exit
 fi
-set -x
 for f in $*
 do
+  prevfs=$(stat --printf="%s" $f)
+  count=5
+  echo "Waiting until $f has the same file size for at least 10 seconds before continuing..."
+  while (( count >= 0 )) ; do
+    sleep 2
+    fs=$(stat --printf="%s" $f)
+    if (( fs == prevfs )) ; then
+      (( --count ))
+    else
+      count=5
+    fi
+    (( prevfs = fs ))
+  done
+  set -x
   bname="$(basename $f)"
   OUT="${bname%.*}-youtube.mp4"
   ffmpeg -y -i "$f" -r 25 \
@@ -26,4 +45,5 @@ do
   -coder 1 -movflags +faststart -x264-params open-gop=0 \
   -c:a libfdk_aac -profile:a aac_low -b:a $ABR \
   "$OUT"
+  set +x
 done
