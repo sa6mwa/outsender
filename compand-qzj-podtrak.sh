bb1f33d62295ecfef442b57af7b3ab538bc0fbc2
SA6MWA Michel <sa6mwa@gmail.com>
2024-08-08 22:44:36 +0200

    Add instructions to compand script

--- compand-qzj-podtrak.sh
+++ compand-qzj-podtrak.sh
@@ -5,6 +5,11 @@ if [ $# -lt 1 ]; then
   echo ""
   echo "This script is intended for a Podtrak MIC wav file with the Rode pod"
   echo "microphone, compand and limit the peak to -2 dB."
+  echo
+  echo "The settings should allow you to have a background stereo track (like music)"
+  echo "below -10 dB. Minus 10.01 dB in fraction is 0.3158639048423471 or 0.31586 if"
+  echo "you can not fit all figures, this should produce a mix without clipping, just"
+  echo "make sure you lower the music to this fraction when the vocal track is on."
   exit
 fi
 set -x
@@ -14,7 +19,7 @@ do
   OUT="companded-${bname%.*}.wav"
   ffmpeg -y -i "$f" -vn -ac 2 -filter_complex "\
 pan=stereo|c0<.5*c0+.5*c1|c1<.5*c0+.5*c1,\
-compand=attacks=.0001:decays=.3:points=-90/-900|-80/-90|-50/-50|-27/-10|0/-2|20/-2:soft-knee=12,\
+compand=attacks=.0001:decays=.5:points=-90/-900|-80/-90|-50/-50|-27/-10|0/-2|20/-2:soft-knee=12,\
 alimiter=limit=0.7943282347242815:level=disabled" \
 "$OUT"
 done
